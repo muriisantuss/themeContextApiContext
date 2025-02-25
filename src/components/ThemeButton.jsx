3a12e70f375b8759945c82b2628c62e341a147a4
muriisantuss <muriisantuss@gmail.com>
2025-02-25 06:18:33 -0300

    :sparkles: feat: 3 modos de cores

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -2,11 +2,21 @@ import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function ToggleButton() {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { toggleTheme, theme } = useContext(ThemeContext);
+
+  let buttonText = "";
+
+  if (theme === "light") {
+    buttonText = "Mudar para Red Mode";
+  } else if (theme === "red") {
+    buttonText = "Mudar para Dark Mode";
+  } else {
+    buttonText = "Mudar para Light Mode";
+  }
 
   return (
     <section>
-      <button onClick={toggleTheme}>Change color backgroundColor</button>
+      <button onClick={toggleTheme}>{buttonText}r</button>
     </section>
   );
 }
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,19 +5,36 @@ export const ThemeContext = createContext();
 
 // Provider para gerenciar o estado do tema
 export function ThemeProvider({ children }) {
-  const [isRedMode, setIsRedMode] = useState(false);
+  const [theme, setTheme] = useState("light");
 
-  const toggleTheme = () => {
-    if (isRedMode) {
-      document.body.style.backgroundColor = "";
-    } else {
-      document.body.style.backgroundColor = "red";
-    }
-    setIsRedMode(!isRedMode);
+  const changeToRedMode = () => {
+    document.body.style.backgroundColor = "red";
+    setTheme("red");
   };
 
+  const changeToDarkMode = () => {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    setTheme("dark");
+  };
+
+  const changeToLightMode = () => {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    setTheme("light");
+  };
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      changeToRedMode();
+    } else if (theme === "red") {
+      changeToDarkMode();
+    } else {
+      changeToLightMode();
+    }
+  };
   return (
-    <ThemeContext.Provider value={{ toggleTheme }}>
+    <ThemeContext.Provider value={{ toggleTheme, theme }}>
       {children}
     </ThemeContext.Provider>
   );
